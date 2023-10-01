3ba5ea6c566349783acf0853fb792fc335399df0
Momtaz Asmar <122457138+Momtaz1992@users.noreply.github.com>
2023-10-01 22:06:05 +0200

    parseFloat and toFixed

--- JavaScript/newProblems.js
+++ JavaScript/newProblems.js
@@ -1,4 +1,4 @@
-// can you find the needle in the haystack
+// can you find the needle in the haystack and which postion of index
 //find the needle at position plus the index 
 
 function findNeedle(haystack) {
@@ -15,3 +15,9 @@ function findNeedle(haystack) {
        return "found the needle at position " + haystack.indexOf("needle")
 }
 console.log(findNeedle(["momtaz","needle","momtaz","needle",]))
+
+const str = "127.456";
+const num = parseFloat(str).toFixed(2);
+console.log(num)
+
+// count of positives / sum of negatives
