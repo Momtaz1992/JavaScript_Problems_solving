6d6b06cfe0aa02bfe266f539ffdf510bb325d61d
Momtaz Asmar <122457138+Momtaz1992@users.noreply.github.com>
2023-10-30 10:19:34 +0100

    delete array

--- JavaScript/array-javascript/concat.js
+++ JavaScript/array-javascript/concat.js
@@ -3,4 +3,4 @@ let ary2 = [4,5,6];
 let ary3 = [7,8,9];
 
 let ary_new = ary1.concat(ary2,ary3);
-console.log(ary_new);
+console.log(ary_new); // [1,2,3,4,5,6,7,8,9] output
---
+++ JavaScript/array-javascript/delete.js
@@ -0,0 +1,5 @@
+// delete array[index];
+
+let fruits = ["banana","apple","grapes"];
+delete fruits[1];
+console.log(fruits) // ["banana",<1 empty item>,"grapes"] output
