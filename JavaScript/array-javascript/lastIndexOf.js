914fc7d7b6ca79a2c9337e3a00f8dc44a0bb70b2
Momtaz Asmar <122457138+Momtaz1992@users.noreply.github.com>
2023-10-30 11:34:38 +0100

    lastOfIndex array

---
+++ JavaScript/array-javascript/lastIndexOf.js
@@ -0,0 +1,9 @@
+// array.lastIndexOf(item, start); Syntax
+
+let elements = ["laptop", "mobile", "headSet", "mobile", "router"];
+
+let indexOf = elements.indexOf("mobile",0);
+let lastIndexOf = elements.lastIndexOf("mobile", 4);
+
+console.log(indexOf); // 1
+console.log(lastIndexOf); // 3
