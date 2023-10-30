eafd83d620b36506e4a500f2c9b8edad5ba0232c
Momtaz Asmar <122457138+Momtaz1992@users.noreply.github.com>
2023-10-30 10:29:14 +0100

    unshift array

---
+++ JavaScript/array-javascript/unshift.js
@@ -0,0 +1,7 @@
+//array.unshift(item1,item2,...itemX) Syntax
+
+let bikes = ["yamaha", "bajaj","honda"];
+let unshifted = bikes.unshift("hero","tvs");
+
+console.log(unshifted); // 5 output
+console.log(bikes) // ["hero","tvs","yamaha", "bajaj","honda"] output
