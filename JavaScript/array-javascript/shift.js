f7db1d6efa2dfea882ddba38d153c9eef175c3fb
Momtaz Asmar <122457138+Momtaz1992@users.noreply.github.com>
2023-10-30 10:23:22 +0100

    shift array

---
+++ JavaScript/array-javascript/shift.js
@@ -0,0 +1,7 @@
+// array.shift() syntax
+
+let bikes = ["yamaha", "bajaj","honda"];
+let shifted = bikes.shift();
+
+console.log(shifted); // yamaha output
+console.log(bikes) // ["bajaj","honda"] output
