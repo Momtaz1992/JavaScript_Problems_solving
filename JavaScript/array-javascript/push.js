5a2250be03b8153eff4bea8d86c58d4e9e8e1700
Momtaz Asmar <122457138+Momtaz1992@users.noreply.github.com>
2023-10-30 10:31:38 +0100

    push  array

---
+++ JavaScript/array-javascript/push.js
@@ -0,0 +1,7 @@
+//array.push(item1,item2,...itemx) Syntax
+
+let bikes = ["yamaha", "bajaj","honda"];
+let pushed = bikes.push("hero","tvs");
+
+console.log(pushed); // 5 output
+console.log(bikes) // ["yamaha", "bajaj","honda","hero","tvs"] output
